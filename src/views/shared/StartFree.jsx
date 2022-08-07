32db65c7e79197232684599eb97412b6e003082d
VidiHawk <fabian@demortier.net>
2022-08-07 17:59:16 +0200

    added particles

--- src/views/shared/StartFree.jsx
+++ src/views/shared/StartFree.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Container, Row, Col } from "reactstrap";
 import { Link } from "react-router-dom";
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
 
 const renderIcons = () => {
   let icons = [];
@@ -28,12 +30,144 @@ const renderIcons = () => {
   return icons;
 };
 
+const particlesInit = async (main) => {
+  await loadFull(main);
+};
+
+const particlesOptions = {
+  fullScreen: {
+    enable: false,
+    zIndex: -1,
+  },
+
+  particles: {
+    number: {
+      value: 80,
+      density: {
+        enable: true,
+        value_area: 800,
+      },
+    },
+    color: {
+      value: "#808080",
+    },
+    shape: {
+      type: "circle",
+      options: {
+        sides: 2,
+      },
+    },
+    opacity: {
+      value: 0.8,
+      random: false,
+      anim: {
+        enable: false,
+        speed: 1,
+        opacity_min: 0.1,
+        sync: false,
+      },
+    },
+    size: {
+      value: 2,
+      random: false,
+      anim: {
+        enable: false,
+        speed: 40,
+        size_min: 0.1,
+        sync: false,
+      },
+    },
+    rotate: {
+      value: 0,
+      random: true,
+      direction: "clockwise",
+      animation: {
+        enable: true,
+        speed: 5,
+        sync: false,
+      },
+    },
+    line_linked: {
+      enable: true,
+      distance: 150,
+      color: "#808080",
+      opacity: 0.4,
+      width: 1,
+    },
+    move: {
+      enable: true,
+      speed: 1,
+      direction: "none",
+      random: false,
+      straight: false,
+      out_mode: "out",
+      attract: {
+        enable: false,
+        rotateX: 600,
+        rotateY: 1200,
+      },
+    },
+  },
+  interactivity: {
+    events: {
+      onhover: {
+        enable: true,
+        mode: ["repulse"],
+      },
+      onclick: {
+        enable: true,
+        mode: "push",
+      },
+      resize: true,
+    },
+    modes: {
+      grab: {
+        distance: 400,
+        line_linked: {
+          opacity: 1,
+        },
+      },
+      bubble: {
+        distance: 400,
+        size: 40,
+        duration: 2,
+        opacity: 8,
+        speed: 3,
+      },
+      repulse: {
+        distance: 150,
+      },
+      push: {
+        particles_nb: 4,
+      },
+      remove: {
+        particles_nb: 2,
+      },
+    },
+  },
+  retina_detect: true,
+  background: {
+    color: "#ffffff",
+    image: "",
+    position: "50% 50%",
+    repeat: "no-repeat",
+    size: "cover",
+  },
+};
+
 const StartFree = () => {
   return (
     <section className="section start-free" id="contact">
-      <div className="shapes-container">{renderIcons()}</div>
+      <div className="tsparticles">
+        <Particles
+          id="tsparticles"
+          init={particlesInit}
+          options={particlesOptions}
+        />
+      </div>
 
-      <Container>
+      <div className="shapes-container">{renderIcons()}</div>
+      <Container className="get-in-touch">
         <Row>
           <Col md="6" className="mx-auto">
             <div className="section-heading text-center">
